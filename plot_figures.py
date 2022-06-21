52374279d71354337efa0887522dd562f99a5e21
bonvech <bonvech@yandex.ru>
2022-06-22 00:09:07 +0300

    one day figure improved

--- plot_figures.py
+++ plot_figures.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from   matplotlib import dates
 import os
@@ -17,7 +18,24 @@ def average_by_three(datum):
         datum.set_index('dt', inplace=True)
         datum.index = pd.to_datetime(datum.index, format='%d.%m.%Y %H:%M') # format='%m/%d%Y %-I%M%S %p'
 
-    return datum.resample("3T").sum().fillna(0).rolling(window=3, min_periods=1).mean()
+    #return datum.resample("3T").sum().fillna().rolling(window=3, min_periods=1).mean()
+    return datum.resample("3T").sum().rolling(window=3, min_periods=1).mean()
+
+
+############################################################################
+## prepare data to plot
+############################################################################ 
+def prepare_data(datum):
+    ## remove nulls from data
+    for x in datum.columns:
+        if "atetime" in x: continue
+        #datum[x][datum[x] == 0] = np.nan
+        mask = datum[x] == 0
+        #datum.loc[mask, x] = np.nan
+        #datum.loc[:, x] = datum[x].replace(0, np.nan)
+        #print(x)
+
+    return datum
 
 
 ############################################################################
@@ -47,6 +65,7 @@ def plot_four_figures_from_excel(xlsfilename, path_to_figures, nfigs=1):
     datum = pd.read_excel(xlsfilename)
     ## take two days data
     data = datum[-2880:]
+    data = prepare_data(data)
     if 'Date' in data.columns:
         x = (data['Date'].astype('string') + ' ' + data['Time (Moscow)'].astype('string'))
         x = pd.to_datetime(x, format='%Y/%m/%d %H:%M:%S')
@@ -54,13 +73,14 @@ def plot_four_figures_from_excel(xlsfilename, path_to_figures, nfigs=1):
         x = data['Datetime'].astype('string')
         x = pd.to_datetime(x, format='%d.%m.%Y %H:%M')
     #print(x)
-    xlims = (x.min(), x.max() + pd.to_timedelta("2:00:00"))
+
 
     # format graph
     plt.rcParams['xtick.labelsize'] = 10
     facecolor = 'white'
     title = xlsfilename.split('/')[-1].split('/')[-1].split('.')[-2].split('_')[-1]
     #title = ae_name
+    xlims = (x.min(), x.max() + pd.to_timedelta("2:00:00"))
 
 
     ##########################
@@ -69,20 +89,22 @@ def plot_four_figures_from_excel(xlsfilename, path_to_figures, nfigs=1):
         fig = plt.figure(figsize=(16, 12))
         ax_1 = fig.add_subplot(3, 1, 1)
     else:
-        fig = plt.figure(figsize=(16, 4))
+        fig = plt.figure(figsize=(10, 5))
         ax_1 = fig.add_subplot(1, 1, 1)
 
     for i in range(7):
         wave = 'BC' + str(i + 1)
-        ax_1.plot(x, data[wave], label=wave)
+        y = data[wave].replace(0, np.nan)
+        ax_1.plot(x, y, label=wave)
     ax_1.xaxis.set_major_formatter(fmt)
     ax_1.set_xlim(xlims)
+    ax_1.set_ylim(bottom=0)
     ax_1.legend()
     ax_1.set_title(title, loc='right')
     ax_1.grid()
     ## save to file "ae33_bc_waves.png"
     if nfigs != 1:
-        fig.savefig(path_to_figures + 'ae33_bc_waves_day.png', facecolor=facecolor) 
+        fig.savefig(path_to_figures + 'ae33_bc_waves_day.svg', facecolor=facecolor, bbox_inches='tight') 
 
 
     ##########################
@@ -90,19 +112,22 @@ def plot_four_figures_from_excel(xlsfilename, path_to_figures, nfigs=1):
     if nfigs == 1:
         ax_2 = fig.add_subplot(3, 1, 2)
     else:
-        fig = plt.figure(figsize=(16, 4))
+        fig = plt.figure(figsize=(10, 5))
         ax_2 = fig.add_subplot(1, 1, 1)
 
-    ax_2.plot(x, data["BCff"], 'k', label='BCff')
-    ax_2.plot(x, data["BCbb"], 'orange', label='BCbb')
+    ax_2.plot(x, data["BCff"].replace(0, np.nan), 'k', label='BCff')
+    ax_2.fill_between(x, data["BCff"].replace(0, np.nan), np.zeros_like(y), color='black')
+    ax_2.plot(x, data["BCbb"].replace(0, np.nan), 'orange', label='BCbb')
+    ax_2.fill_between(x, data["BCbb"].replace(0, np.nan), np.zeros_like(y), color='orange')
     ax_2.xaxis.set_major_formatter(fmt)
     ax_2.set_xlim(xlims)
+    ax_2.set_ylim(bottom=0)
     ax_2.legend()
     ax_2.grid()
     ## save to file "ae33_bc.png"
     if nfigs != 1:
         ax_2.set_title(title, loc='right')
-        fig.savefig(path_to_figures + 'ae33_bc_day.png', facecolor=facecolor) 
+        fig.savefig(path_to_figures + 'ae33_bc_day.svg', facecolor=facecolor, bbox_inches='tight') 
 
 
     #####################################
@@ -118,7 +143,7 @@ def plot_four_figures_from_excel(xlsfilename, path_to_figures, nfigs=1):
         fmt = dates.DateFormatter('%d/%m/%Y')
 
     plt.rcParams['xtick.labelsize'] = 8
-
+    print(data.index.min(), data.index.max(), "delta:", data.index.max() - data.index.min())
 
     ##########################
     ## Figure 3: 
@@ -131,9 +156,11 @@ def plot_four_figures_from_excel(xlsfilename, path_to_figures, nfigs=1):
 
     for i in range(7):
         wave = 'BC' + str(i + 1)
-        ax_3.plot(data[wave], label=wave)
+        xx = data[wave].replace(0, np.nan)
+        ax_3.plot(xx, label=wave)
     ax_3.xaxis.set_major_formatter(fmt)
-    #ax_3.set_xlim(xx.min(), xx.max())
+    ax_3.set_xlim(left=xx.index.min())
+    ax_3.set_ylim(bottom=0)
     ax_3.legend() # ncol = 7, fontsize = 9)
     ax_3.grid()
     ## save to file "ae33_bc_waves_week.png"
@@ -150,12 +177,15 @@ def plot_four_figures_from_excel(xlsfilename, path_to_figures, nfigs=1):
         fig = plt.figure(figsize=(8, 4))
         ax_4 = fig.add_subplot(1, 1, 1)
 
-    yy = data["BCff"]
-    zz = data["BCbb"]
+    yy = data["BCff"].replace(0, np.nan)
+    zz = data["BCbb"].replace(0, np.nan)
     ax_4.plot(yy, 'k', label='BCff')
+    ax_4.fill_between(yy.index, yy, np.zeros_like(yy), color='black')
     ax_4.plot(zz, 'orange', label='BCbb')
+    ax_4.fill_between(zz.index, zz, np.zeros_like(zz), color='orange')
     ax_4.xaxis.set_major_formatter(fmt)
-    #ax_4.set_xlim(xx.min(), xx.max())
+    ax_4.set_xlim(left=zz.index.min())
+    ax_4.set_ylim(bottom=0)
     ax_4.legend()
     ax_4.grid()
 
