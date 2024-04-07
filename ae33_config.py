9fd28a7024700d07e0914247418950c9a8a416cf
bonvech <bonvech@yandex.ru>
2024-04-07 12:35:29 +0300

    config

---
+++ ae33_config.py
@@ -0,0 +1,19 @@
+# Programm mode:
+#   1 - for MAIN-menu,  0 - Auto RUN
+#
+RUN=1
+#
+# Directorry for DATA:
+#
+.\data\
+#
+#
+# AE33:   IP address and Port:
+#
+IP=192.168.1.37  8002
+#
+#
+# AE33:  Last Records:
+#
+MINID=943370
+#
